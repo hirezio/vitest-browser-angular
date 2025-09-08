70cd6c657fc3de94025f3e19af189cfb66691c90
Shai <shairez@users.noreply.github.com>
2025-09-08 03:21:30 +0300

    removed unnecessary optimization

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  optimizeDeps: { include: ['vitest-browser-angular/setup-zones'] },
   test: {
     globals: true,
     setupFiles: ['./src/setup-zones.ts'],
